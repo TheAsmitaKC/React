f998bb2c6dff2eaa2d9c88590782b18f7306cc08
theasmitakc <theasmitakc@gmail.com>
2025-07-04 21:21:16 +0545

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,33 @@
+import React, { useState } from 'react'
+
+const App = () => {
+  const[text, settext]= useState('')
+  const[comment, setcomment]= useState('')
+  const handlebutton=(e)=>{
+   const textval = e.target.innerText;
+   settext(textval)
+   console.log(textval)
+  }
+  const handlesubmit=()=>{
+setcomment(text)
+  }
+  return (
+    <div className=' flex flex-col justify-center h-[100vh] items-center'>
+      <div className='bg-amber-200 h-[60vh] w-[30vw] flex flex-col items-center'>
+        <div  className='mt-10 '>
+       <button className='border p-2 m-5 bg-white hover:bg-amber-100 ' onClick={handlebutton}  >Beautiful</button>
+       <button className='border p-2 m-5  bg-white hover:bg-amber-100 'onClick={handlebutton}>Wonderful</button>
+       <button className='border p-2 m-5  bg-white hover:bg-amber-100 ' onClick={handlebutton}>Gorgeous</button>
+       <button className='border p-2 m-5  bg-white hover:bg-amber-100 'onClick={handlebutton}>Nice</button>
+       </div>
+       <input  className=' border p-4 w-[20vw] m-10 bg-white'type="text" placeholder='Enter comment ...'  value={text}  onChange={(e)=>e.target.value}/>
+       <button className='border p-2 m-5 bg-white hover:bg-amber-100 w-50 ' onClick={handlesubmit}>Send Comment</button>
+       {comment && <p>
+        Submitted comment is :{comment}
+       </p> }
+      </div>
+    </div>
+  )
+}
+
+export default App
