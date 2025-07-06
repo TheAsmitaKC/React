131cf00afdf5452378fb6cc8bc8b34c299f8bc52
theasmitakc <theasmitakc@gmail.com>
2025-07-06 17:04:56 +0545

    probleminlocalstorage

--- src/TaskList/task.jsx
+++ src/TaskList/task.jsx
@@ -3,38 +3,37 @@ import React, {  useEffect, useState } from 'react'
 const task = () => {
     const [text, settext]= useState([''])
     const [task, settask] = useState([])
-    const handleaddbutton=()=>{
-   
-      settask((prev)=>[...prev,text])
+    const handlesubmit=(e)=>{
+e.preventDefault();
+localStorage.setItem("task",JSON.stringify(task));
 
-      settext("")
-  
+settask((prev)=>[...prev,text]);
+settext("")
     }
-  localStorage.setItem('task',JSON.stringify( task))
-
-    // useEffect(()=>{
-    //   const task = JSON.parse(localStorage.getItem('task'));
-    //   if(task){
-    //     settask(task)
-    //   }
-    // })
-  
+    useEffect(()=>{
+      const saved = localStorage.getItem('tasks');
+    if (saved) {
+      settask(JSON.parse(saved));
+    }
+    },[])
+    
+    console.log(task.length)
   return (
     <div className=' flex flex-col justify-center h-[100vh] items-center'>
     <div className='bg-amber-200 h-[60vh] w-[30vw] flex flex-col items-center'>
       <h1 className='text-3xl mt-10'>Add Task</h1>  
-      <div  className='mt-10  '>
+      <form  className='mt-10  ' onSubmit={handlesubmit}>
         
       <input  className=' border p-3 w-[20vw]  bg-white'type="text" placeholder='Enter comment ...' value={text} onChange={(e)=>settext(e.target.value)} />
      
-    <button className=' border p-3  ml-5 mb-10 bg-white' onClick={handleaddbutton}>Add</button>
+    <button className=' border p-3  ml-5 mb-10 bg-white' type='submit'>Add</button>
     
     <h1 className='text-2xl'>Task List</h1>
     <ul>{task.length ===0 && <li>No tasks found</li>}
     {task.length >0 && task.map((item, index)=><li  key={index}>{item}</li>)}
     </ul>
 
-  </div>
+  </form>
     </div>
   </div>
   )
